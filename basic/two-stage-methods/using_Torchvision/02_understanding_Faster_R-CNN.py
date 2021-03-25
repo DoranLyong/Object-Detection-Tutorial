7edc4cfcde95b4faf26548214f3ef7719e53e435
DoranLyong <cheeryun@gmail.com>
2021-03-26 01:04:48 +0900

    README

--- basic/two-stage-methods/using_Torchvision/02_understanding_Faster_R-CNN.py
+++ basic/two-stage-methods/using_Torchvision/02_understanding_Faster_R-CNN.py
@@ -9,7 +9,7 @@
 
 #%% 임포트 패키지  
 import os.path as osp
-import os
+import os 
 
 import numpy as np 
 import cv2 
@@ -17,11 +17,10 @@ from PIL import Image  # 파이썬용 이미지를 처리하는 패키지 ; (ref
 from tqdm import tqdm 
 import matplotlib.pyplot as plt
 import torch 
-import torch.nn as nn  # 학습 가능한 레이어들을 담은 패키지 ; # All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
-import torch.backends.cudnn as cudnn    # https://hoya012.github.io/blog/reproducible_pytorch/
-                                        # https://stackoverflow.com/questions/58961768/set-torch-backends-cudnn-benchmark-true-or-not
-import torch.optim as optim  # 최적화 알고리즘을 담은 패키지 ; # For all Optimization algorithms, SGD, Adam, etc.
-from torch.utils.data import DataLoader   # Gives easier dataset management and creates mini batches
 import torchvision
-import torchvision.datasets as datasets  # 이미지 데이터를 불러오고 변환하는 패키지 ;  # Has standard datasets we can import in a nice way
 import torchvision.transforms as transforms  # Transformations we can perform on our dataset
+
+
+cwd =  osp.dirname(osp.realpath(__file__))  # 현재 파일의 디렉토리 경로 가져오기; 
+                                                # (ref) https://stackoverflow.com/questions/5137497/find-current-directory-and-files-directory
+data_path = osp.join(cwd, 'data')
--- basic/two-stage-methods/using_Torchvision/README.md
+++ basic/two-stage-methods/using_Torchvision/README.md
@@ -1,1 +1,23 @@
 # Faster R-CNN using Torchvision
+
+1. `Torchvision` 을 사용해서 Faster R-CNN 사용해보기 (inference mode)
+2. Faster R-CNN 구조 이해하기 (using `Torchvision`)
+3. Faster R-CNN Fine-tuning (data class 바꾸기)
+4. Faster R-CNN Fine-tuning Training
+
+<br/>
+
+***
+## `Torchvision` 을 사용해서 Faster R-CNN 사용해보기 (inference mode)
+``` bash
+~$ python 01_Faster_R-CNN_using_Torchvision.py
+```
+
+
+<br/>
+
+
+## Faster R-CNN 구조 이해하기 (using `Torchvision`)
+```bash
+~$ python 02_understanding_Faster_R-CNN.py
+```
